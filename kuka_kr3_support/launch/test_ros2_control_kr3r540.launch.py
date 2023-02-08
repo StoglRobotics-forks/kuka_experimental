36100861e3714d64f1692a8c742ef86fb1871783
GiridharBukka <giridhar6937@gmail.com>
2023-02-08 12:43:05 +0100

    addng .py extension as required in foxy

--- kuka_kr3_support/launch/test_ros2_control_kr3r540.launch.py
+++ kuka_kr3_support/launch/test_ros2_control_kr3r540.launch.py
@@ -79,14 +79,14 @@ def generate_launch_description():
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
-        executable="spawner",
+        executable="spawner.py",
         arguments=["joint_state_broadcaster",
                    "--controller-manager", "/controller_manager"],
     )
 
     robot_controller_spawner = Node(
         package="controller_manager",
-        executable="spawner",
+        executable="spawner.py",
         arguments=["forward_position_controller", "-c", "/controller_manager"],
     )
 
