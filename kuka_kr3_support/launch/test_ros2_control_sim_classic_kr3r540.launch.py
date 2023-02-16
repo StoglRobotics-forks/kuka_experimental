8f85f81ef2c858e8071bdf391f12474538573da6
GiridharBukka <giridhar6937@gmail.com>
2023-02-16 14:30:09 +0100

    correction

--- kuka_kr3_support/launch/test_ros2_control_sim_classic_kr3r540.launch.py
+++ kuka_kr3_support/launch/test_ros2_control_sim_classic_kr3r540.launch.py
@@ -30,8 +30,8 @@ def generate_launch_description():
                 ])
         ),
         launch_arguments={
-            "description_package": "kuka_kr10_support",
-            "description_file": "kr10r900_2.xacro",
+            "description_package": "kuka_kr3_support",
+            "description_file": "kr3r540.xacro",
         }.items(),
     )
     
