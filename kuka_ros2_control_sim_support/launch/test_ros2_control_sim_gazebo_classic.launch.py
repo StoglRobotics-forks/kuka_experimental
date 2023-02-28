5684a3e432aecbe6038a33ebfbf46c1c1995ba23
GiridharBukka <giridhar6937@gmail.com>
2023-02-28 10:31:25 +0100

    functional kuka simulators with ros2_control support

--- kuka_ros2_control_sim_support/launch/test_ros2_control_sim_gazebo_classic.launch.py
+++ kuka_ros2_control_sim_support/launch/test_ros2_control_sim_gazebo_classic.launch.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+# Copyright 2023 Stogl Robotics Consulting UG (haftungsbeschränkt)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -48,8 +48,7 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "description_package",
-            default_value="kuka_kr5_support",
+            "robot_description_package",
             choices=[
                 "kuka_kr3_support", 
                 "kuka_kr5_support", 
@@ -66,22 +65,21 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "description_file",
-            default_value="kr5_arc.xacro",
+            "robot_description_macro_file",
             choices=[
-                "kr3r540.xacro", 
-                "kr5_arc.xacro", 
-                "kr6r700sixx.xacro", 
-                "kr6r900_2.xacro", 
-                "kr6r900sixx.xacro",
-                "kr10r900_2.xacro",
-                "kr10r1100sixx.xacro",
-                "kr10r1420.xacro",                
-                "kr16_2.xacro", 
-                "kr120r2500pro.xacro", 
-                "kr150_2.xacro", 
-                "kr150r3100_2.xacro", 
-                "kr210l150.xacro",
+                "kr3r540_macro.xacro", 
+                "kr5_arc_macro.xacro", 
+                "kr6r700sixx_macro.xacro", 
+                "kr6r900_2_macro.xacro", 
+                "kr6r900sixx_macro.xacro",
+                "kr10r900_2_macro.xacro",
+                "kr10r1100sixx_macro.xacro",
+                "kr10r1420_macro.xacro",                
+                "kr16_2_macro.xacro", 
+                "kr120r2500pro_macro.xacro", 
+                "kr150_2_macro.xacro", 
+                "kr150r3100_2_macro.xacro", 
+                "kr210l150_macro.xacro",
                 ],
             description="URDF/XACRO description file with the robot.",
         )
@@ -89,7 +87,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "prefix",
-            default_value='""',
+            default_value="",
             description="Prefix of the joint names, useful for \
         multi-robot setup. If changed than also joint names in the controllers' configuration \
         have to be updated.",
@@ -103,13 +101,64 @@ def generate_launch_description():
             description="Robot controller to start.",
         )
     )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_mock_hardware",
+            default_value="true",
+            description="Start robot with fake hardware mirroring command to its states.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ros2_control_sim_support_package",
+            default_value="kuka_ros2_control_sim_support",
+            description="Support package for ros2_control and simulators. Contains files that use base "
+            "kuka robot description and add on ros2_control support with gazebo simulators. ",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "common_sim_description_file",
+            default_value="common_sim_kuka.xacro",
+            description="loads robot macros by chosen value for arguments 'robot_description_package'"
+            "'robot_description_file' and 'robot_name'. Also loads ros2_control with simulators support .",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_name",
+            choices=[
+                "kuka_kr3r540", 
+                "kuka_kr5_arc", 
+                "kuka_kr6r700sixx", 
+                "kuka_kr6r900_2", 
+                "kuka_kr6r900sixx",
+                "kuka_kr10r900_2",
+                "kuka_kr10r1100sixx",
+                "kuka_kr10r1420",                
+                "kuka_kr16_2", 
+                "kuka_kr120r2500pro", 
+                "kuka_kr150_2", 
+                "kuka_kr150r3100_2", 
+                "kuka_kr210l150",
+                ],
+            description="NOTE:robot name and robot description macro name are same",
+        )
+    )
+    
+    
+    
 
     # Initialize Arguments
     runtime_config_package = LaunchConfiguration("runtime_config_package")
     controllers_file = LaunchConfiguration("controllers_file")
-    description_package = LaunchConfiguration("description_package")
-    description_file = LaunchConfiguration("description_file")
+    robot_description_package = LaunchConfiguration("robot_description_package")
+    ros2_control_sim_support_package = LaunchConfiguration("ros2_control_sim_support_package")
+    common_sim_description_file = LaunchConfiguration("common_sim_description_file")
+    robot_description_macro_file = LaunchConfiguration("robot_description_macro_file")
+    robot_name = LaunchConfiguration("robot_name")
     prefix = LaunchConfiguration("prefix")
+    use_mock_hardware = LaunchConfiguration("use_mock_hardware")
     robot_controller = LaunchConfiguration("robot_controller")
 
     robot_controllers = PathJoinSubstitution(
@@ -126,7 +175,7 @@ def generate_launch_description():
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare(description_package), "urdf", description_file]
+                [FindPackageShare(ros2_control_sim_support_package), "urdf", common_sim_description_file]
             ),
             " ",
             "prefix:=",
@@ -142,6 +191,15 @@ def generate_launch_description():
             " ",
             "simulation_controllers:=",
             robot_controllers,
+            " ",
+            "robot_description_package:=",
+            robot_description_package,
+            " ",
+            "robot_description_macro_file:=",
+            robot_description_macro_file,
+            " ",
+            "robot_name:=",
+            robot_name,
             " ",
         ]
     )
