94ba8a061b861f48b5051829cb65c5b14d356396
Denis Štogl <denis@stoglrobotics.de>
2023-05-15 14:15:59 +0200

    Optimized joint limits and allowed duration of trajectory execution increased to use scaling controller.

--- kuka_common_moveit/launch/bringup.launch.py
+++ kuka_common_moveit/launch/bringup.launch.py
@@ -341,7 +341,7 @@ def generate_launch_description():
 
     trajectory_execution = {
         "moveit_manage_controllers": False,
-        "trajectory_execution.allowed_execution_duration_scaling": 1.2,
+        "trajectory_execution.allowed_execution_duration_scaling": 100.0,
         "trajectory_execution.allowed_goal_duration_margin": 0.5,
         "trajectory_execution.allowed_start_tolerance": 0.01,
     }
